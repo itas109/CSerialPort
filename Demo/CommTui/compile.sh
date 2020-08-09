2ec45a3b485992cf7d5487836978dcd91651600f
itas109 <itas109@qq.com>
2020-08-09 23:17:37 +0800

    [add] CommTui linux compile.sh
    Signed-off-by: itas109 <itas109@qq.com>

---
+++ Demo/CommTui/compile.sh
@@ -0,0 +1,3 @@
+#! /bin/bash
+
+g++ CSerialPortDemoTui.cpp tui.c ../../src/SerialPortInfo.cpp ../../src/SerialPortInfoBase.cpp ../../src/SerialPortInfoUnixBase.cpp ../../src/SerialPort.cpp ../../src/SerialPortBase.cpp ../../src/SerialPortUnixBase.cpp -lpthread -o CSerialPortDemoTui -I. -I../../src -L. -lncurses
