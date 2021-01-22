e363df1572525d041c0320d122a0e872a1049edf
itas109 <itas109@qq.com>
2021-01-23 00:19:40 +0800

    [fixed] fixed #41 - some win10 could not enum all avaiable ports with function availablePortInfos()
    Signed-off-by: itas109 <itas109@qq.com>

--- src/SerialPortInfoWinBase.cpp
+++ src/SerialPortInfoWinBase.cpp
@@ -39,7 +39,7 @@ std::string wstringToString(const std::wstring &wstr)
  */
 bool enumDetailsSerialPorts(vector<SerialPortInfo> &portInfoList)
 {
-    // https://docs.microsoft.com/en-us/windows/win32/api/setupapi/
+    // https://docs.microsoft.com/en-us/windows/win32/api/setupapi/nf-setupapi-setupdienumdeviceinfo
 
     bool bRet = false;
     SerialPortInfo m_serialPortInfo;
@@ -48,7 +48,6 @@ bool enumDetailsSerialPorts(vector<SerialPortInfo> &portInfoList)
     std::string strPortName;
 
     HDEVINFO hDevInfo = INVALID_HANDLE_VALUE;
-    SP_DEVICE_INTERFACE_DETAIL_DATA *pDetData = NULL;
 
     // Return only devices that are currently present in a system
     // The GUID_DEVINTERFACE_COMPORT device interface class is defined for COM ports. GUID
@@ -57,99 +56,49 @@ bool enumDetailsSerialPorts(vector<SerialPortInfo> &portInfoList)
 
     if (INVALID_HANDLE_VALUE != hDevInfo)
     {
-        BOOL bOk = TRUE;
+        SP_DEVINFO_DATA devInfoData;
+        // The caller must set DeviceInfoData.cbSize to sizeof(SP_DEVINFO_DATA)
+        devInfoData.cbSize = sizeof(SP_DEVINFO_DATA);
 
-        DWORD dwDetDataSize = sizeof(SP_DEVICE_INTERFACE_DETAIL_DATA) + 256;
-        pDetData = (SP_DEVICE_INTERFACE_DETAIL_DATA *)new char[dwDetDataSize];
-
-        if (pDetData != NULL)
+        for (DWORD i = 0; SetupDiEnumDeviceInfo(hDevInfo, i, &devInfoData); i++)
         {
-            pDetData->cbSize = sizeof(SP_DEVICE_INTERFACE_DETAIL_DATA);
-
-            SP_DEVICE_INTERFACE_DATA did = {0};
-            did.cbSize = sizeof(did);
-
-            for (DWORD idev = 0; bOk; idev++)
+            // get port name
+            TCHAR portName[256];
+            HKEY hDevKey = SetupDiOpenDevRegKey(hDevInfo, &devInfoData, DICS_FLAG_GLOBAL, 0, DIREG_DEV, KEY_READ);
+            if (INVALID_HANDLE_VALUE != hDevKey)
             {
-                // enumerates the device interfaces that are contained in a device information set
-                bOk = SetupDiEnumDeviceInterfaces(hDevInfo, NULL, &GUID_DEVINTERFACE_COMPORT, idev, &did);
-                if (bOk)
-                {
-                    // DWORD cbRequired = 0;
-
-                    SP_DEVINFO_DATA devdata = {sizeof(SP_DEVINFO_DATA)};
-                    // get detailed information about an interface
-                    // bOk = SetupDiGetDeviceInterfaceDetail(hDevInfo, &did, NULL, NULL,&cbRequired, &devdata);
-
-                    bOk = SetupDiGetDeviceInterfaceDetail(hDevInfo, &did, pDetData, dwDetDataSize, NULL, &devdata);
-
-                    if (bOk)
-                    {
-                        TCHAR fname[256];
-                        // retrieves a specified Plug and Play device property (include friendlyname etc.)
-                        BOOL bSuccess = SetupDiGetDeviceRegistryProperty(hDevInfo, &devdata, SPDRP_FRIENDLYNAME, NULL,
-                                                                         (PBYTE)fname, sizeof(fname), NULL);
-
-                        TCHAR portName[256];
-                        // get port name
-                        HKEY hDevKey =
-                            SetupDiOpenDevRegKey(hDevInfo, &devdata, DICS_FLAG_GLOBAL, 0, DIREG_DEV, KEY_READ);
-                        if (INVALID_HANDLE_VALUE != hDevKey)
-                        {
-                            DWORD dwCount = 255; // DEV_NAME_MAX_LEN
-                            RegQueryValueEx(hDevKey, _T("PortName"), NULL, NULL, (BYTE *)portName, &dwCount);
-                            RegCloseKey(hDevKey);
-
-                            bSuccess &= TRUE;
-                        }
-                        else
-                        {
-                            bSuccess &= FALSE;
-                        }
-
-                        if (bSuccess)
-                        {
-#ifdef UNICODE
-                            strPortName = wstringToString(portName);
-                            strFriendlyName = wstringToString(fname);
-#else
-                            strPortName = std::string(portName);
-                            strFriendlyName = std::string(fname);
-#endif
-                            // remove (COMxx)
-                            strFriendlyName = strFriendlyName.substr(0, strFriendlyName.find(("(COM")));
-
-                            m_serialPortInfo.portName = strPortName;
-                            m_serialPortInfo.description = strFriendlyName;
-                            portInfoList.push_back(m_serialPortInfo);
-
-                            bRet = true;
-                        }
-                    }
-                    else
-                    {
-                        bRet = false;
-                    }
-                }
-                else if (ERROR_NO_MORE_ITEMS == GetLastError())
-                {
-                    bRet = false; // Enumeration failed
-                    break;
-                }
-                else
-                {
-                }
+                DWORD dwCount = 255; // DEV_NAME_MAX_LEN
+                RegQueryValueEx(hDevKey, _T("PortName"), NULL, NULL, (BYTE *)portName, &dwCount);
+                RegCloseKey(hDevKey);
             }
 
-            delete[](char *) pDetData;
-            pDetData = NULL;
+            // get friendly name
+            TCHAR fname[256];
+            SetupDiGetDeviceRegistryProperty(hDevInfo, &devInfoData, SPDRP_FRIENDLYNAME, NULL, (PBYTE)fname,
+                                             sizeof(fname), NULL);
+
+#ifdef UNICODE
+            strPortName = wstringToString(portName);
+            strFriendlyName = wstringToString(fname);
+#else
+            strPortName = std::string(portName);
+            strFriendlyName = std::string(fname);
+#endif
+            // remove (COMxx)
+            strFriendlyName = strFriendlyName.substr(0, strFriendlyName.find(("(COM")));
+
+            m_serialPortInfo.portName = strPortName;
+            m_serialPortInfo.description = strFriendlyName;
+            portInfoList.push_back(m_serialPortInfo);
         }
-        else
+
+        if (ERROR_NO_MORE_ITEMS == GetLastError())
         {
-            bRet = false;
+            bRet = true; // no more item
         }
-        SetupDiDestroyDeviceInfoList(hDevInfo);
     }
+
+    SetupDiDestroyDeviceInfoList(hDevInfo);
 
     return bRet;
 }
