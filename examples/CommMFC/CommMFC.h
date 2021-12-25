209dbe7e7096f6ab9a28051340b937c8491139e5
itas109 <itas109@qq.com>
2021-12-25 17:06:50 +0800

    modify CommMFC example with cmake, deafult vs2008
    Signed-off-by: itas109 <itas109@qq.com>

---
+++ examples/CommMFC/CommMFC.h
@@ -0,0 +1,31 @@
+// CommMFC.h : PROJECT_NAME 应用程序的主头文件
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "在包含此文件之前包含“stdafx.h”以生成 PCH 文件"
+#endif
+
+#include "resource.h"		// 主符号
+
+
+// CCommMFCApp:
+// 有关此类的实现，请参阅 CommMFC.cpp
+//
+
+class CCommMFCApp : public CWinApp
+{
+public:
+	CCommMFCApp();
+
+// 重写
+	public:
+	virtual BOOL InitInstance();
+
+// 实现
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CCommMFCApp theApp;
---
+++ examples/CommMFC/Resource.h
@@ -0,0 +1,38 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by CommMFC.rc
+//
+#define IDR_MAINFRAME					128
+#define IDM_ABOUTBOX					0x0010
+#define IDD_ABOUTBOX					100
+#define IDS_ABOUTBOX					101
+#define IDD_COMMMFC_DIALOG				102
+#define IDR_MAINFRAME                   128
+#define IDC_BUTTON_OPEN_CLOSE           1000
+#define IDC_COMBO_PORT_Nr               1001
+#define IDC_COMBO_BAUDRATE              1002
+#define IDC_EDIT1                       1003
+#define IDC_ReceiveEdit                 1003
+#define IDC_SendEdit                    1004
+#define IDC_BUTTON_SEND                 1005
+#define IDC_BUTTON_TIMER                1006
+#define IDC_STATIC_RECV_COUNT           1006
+#define IDC_STATIC_RECV_COUNT_VALUE     1007
+#define IDC_STATIC_SEND_COUNT           1008
+#define IDC_STATIC_SEND_COUNT_VALUE     1009
+#define IDC_BUTTON_CLEAR                1010
+#define IDC_COMBO_PARITY                1011
+#define IDC_COMBO_STOP                  1012
+#define IDC_COMBO_DATABITS              1013
+
+// 新对象的下一组默认值
+//
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+
+#define _APS_NEXT_RESOURCE_VALUE	129
+#define _APS_NEXT_CONTROL_VALUE		1000
+#define _APS_NEXT_SYMED_VALUE		101
+#define _APS_NEXT_COMMAND_VALUE		32771
+#endif
+#endif
