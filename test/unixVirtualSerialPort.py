886ead9a381b936b4ae65b6795d0d22bb0cc09d9
itas109 <itas109@qq.com>
2022-10-01 19:00:04 +0800

    test(unixVirtualSerialPort): unix virtual serial port (python 2.x 3.x)
    Signed-off-by: itas109 <itas109@qq.com>

--- test/unixVirtualSerialPort.py
+++ test/unixVirtualSerialPort.py
@@ -1,6 +1,8 @@
 #! /usr/bin/env python
 #coding=utf-8
 
+from __future__ import print_function # python2 use print() function
+
 import pty
 import os
 import select
@@ -11,17 +13,18 @@ def mkpty():
     slaveName1 = os.ttyname(slave)
     master2, slave = pty.openpty()
     slaveName2 = os.ttyname(slave)
-    print '\nslave device names: ', slaveName1, slaveName2
-    return master1, master2
+    print('virtual serial port names: ', slaveName1, slaveName2)
+    return master1, master2, slaveName1, slaveName2
 
 if __name__ == "__main__":
-    master1, master2 = mkpty()
+    master1, master2, slaveName1, slaveName2 = mkpty()
     while True:
         rl, wl, el = select.select([master1,master2], [], [], 1)
         for master in rl:
-            data = os.read(master, 128)
-            print "read %d data." % len(data)
+            data = os.read(master, 1024)
             if master==master1:
+                print('from %s - [%d] %s' %(slaveName1, len(data), data))
                 os.write(master2, data)
             else:
+                print('from %s - [%d] %s' %(slaveName2, len(data), data))
                 os.write(master1, data)
