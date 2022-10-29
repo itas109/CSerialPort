56f38d527611b2ed509e515b33dff7cba02b3f61
itas109 <itas109@qq.com>
2022-10-29 21:58:57 +0800

    refactor(CommNoGui): modify according to the code review

--- examples/CommNoGui/CSerialPortDemoNoGui.cpp
+++ examples/CommNoGui/CSerialPortDemoNoGui.cpp
@@ -1,8 +1,5 @@
 #include <iostream>
 
-#include "CSerialPort/SerialPort.h"
-#include "CSerialPort/SerialPortInfo.h"
-
 #ifdef _WIN32
 #include <windows.h>
 #define imsleep(microsecond) Sleep(microsecond) // ms
@@ -12,73 +9,75 @@
 #endif
 
 #include <vector>
+
+#include "CSerialPort/SerialPort.h"
+#include "CSerialPort/SerialPortInfo.h"
 using namespace itas109;
-using namespace std;
+
+std::string char2hexstr(const char *str, int len)
+{
+    static const char hexTable[] = "0123456789ABCDEF";
+
+    std::string result;
+    for (int i = 0; i < len; ++i)
+    {
+        result += "0x";
+        result += hexTable[str[i] / 16];
+        result += hexTable[str[i] % 16];
+        result += " ";
+    }
+    return result;
+}
 
 int countRead = 0;
 
-class mySlot : public has_slots<>
+class MyListener : public has_slots<>
 {
 public:
-    mySlot(CSerialPort *sp)
+    MyListener(CSerialPort *sp)
+        : p_sp(sp)
+        , recLen(0)
     {
-        recLen = -1;
-        p_sp = sp;
+        memset(data, 0, 1024);
     };
 
-    void OnSendMessage()
+    void onReadEvent()
     {
         // read
-        recLen = p_sp->readAllData(str);
+        recLen = p_sp->readAllData(data);
 
         if (recLen > 0)
         {
-            countRead++;
+            recLen = recLen < 1024 ? recLen : 1024; // suppose receive length less than 1024
+            data[recLen] = '\0';
+            std::cout << "Count: " << ++countRead << ", Length: " << recLen << ", Str: " << data << ", Hex: " << char2hexstr(data, recLen).c_str() << std::endl;
 
-            str[recLen] = '\0';
-            std::cout << "receive data : " << str << ", receive size : " << recLen << ", receive count : " << countRead << std::endl;
-
-            if (countRead > 7)
-            {
-                std::cout << "close serial port when receive count > 7" << std::endl;
-                p_sp->close();
-            }
-            else
-            {
-                // return receive data
-                p_sp->writeData(str, recLen);
-            }
+            // return receive data
+            p_sp->writeData(data, recLen);
         }
     };
 
 private:
-    mySlot(){};
-
-private:
     CSerialPort *p_sp;
 
-    char str[1024];
+    char data[1024];
     int recLen;
 };
 
 int main()
 {
-    size_t index = -1;
-    std::string portName;
-    vector<SerialPortInfo> m_availablePortsList;
     CSerialPort sp;
+    std::cout << "Version: " << sp.getVersion() << std::endl << std::endl;
 
-    std::cout << "Version : " << sp.getVersion() << std::endl << std::endl;
+    MyListener listener(&sp);
 
-    mySlot receive(&sp);
+    std::vector<SerialPortInfo> m_availablePortsList = CSerialPortInfo::availablePortInfos();
 
-    m_availablePortsList = CSerialPortInfo::availablePortInfos();
+    std::cout << "availableFriendlyPorts: " << std::endl;
 
-    std::cout << "availableFriendlyPorts : " << std::endl;
-
-    for (size_t i = 0; i < m_availablePortsList.size(); i++)
+    for (size_t i = 1; i <= m_availablePortsList.size(); ++i)
     {
-        std::cout << i << " - " << m_availablePortsList[i].portName << " " << m_availablePortsList[i].description << std::endl;
+        std::cout << i << " - " << m_availablePortsList[i - 1].portName << " " << m_availablePortsList[i - 1].description << std::endl;
     }
 
     if (m_availablePortsList.size() == 0)
@@ -89,43 +88,48 @@ int main()
     {
         std::cout << std::endl;
 
+        int input = -1;
         do
         {
-            std::cout << "Please input index of the port(0 - " << (m_availablePortsList.size() - 1) << " ) : " << std::endl;
+            std::cout << "Please Input The Index Of Port(1 - " << m_availablePortsList.size() << ")" << std::endl;
 
-            std::cin >> index;
+            std::cin >> input;
 
-            if (index >= 0 && index < m_availablePortsList.size())
+            if (input >= 1 && input <= m_availablePortsList.size())
             {
                 break;
             }
         } while (true);
 
-        portName = m_availablePortsList[index].portName;
-        std::cout << "select port name : " << portName << std::endl;
+        std::string portName = m_availablePortsList[input - 1].portName;
+        std::cout << "Port Name: " << portName << std::endl;
 
-        sp.init(portName, // windows:COM1 Linux:/dev/ttyS0
-                itas109::BaudRate9600, 
-                itas109::ParityNone, 
-                itas109::DataBits8, 
-                itas109::StopOne
-                );
+        sp.init(portName,              // windows:COM1 Linux:/dev/ttyS0
+                itas109::BaudRate9600, // baudrate
+                itas109::ParityNone,   // parity
+                itas109::DataBits8,    // data bit
+                itas109::StopOne,      // stop bit
+                itas109::FlowNone,     // flow
+                4096                   // read buffer size
+        );
+        sp.setReadIntervalTimeout(30); // read interval timeout 30ms
 
         sp.open();
-
-        if (sp.isOpened())
-        {
-            std::cout << "open " << portName << " success" << std::endl;
-        }
-        else
-        {
-            std::cout << "open " << portName << " failed" << std::endl;
-        }
+        std::cout << "Open " << portName << (sp.isOpened() ? " Success" : " Failed") << std::endl;
 
         // connect for read
-        sp.readReady.connect(&receive, &mySlot::OnSendMessage);
+        sp.readReady.connect(&listener, &MyListener::onReadEvent);
 
-        // write
+        // write hex data
+        char hex[5];
+        hex[0] = 0x31;
+        hex[1] = 0x32;
+        hex[2] = 0x33;
+        hex[3] = 0x34;
+        hex[4] = 0x35;
+        sp.writeData(hex, sizeof(hex));
+
+        // write str data
         sp.writeData("itas109", 7);
 
         for (;;)
