3a1c6ee7b29cb81d48d7456fee213d83e70eefcc
itas109 <itas109@qq.com>
2022-11-20 13:56:03 +0800

    test(unitTest): replace isOpened() with isOpen()

--- test/cseiralport_test.cpp
+++ test/cseiralport_test.cpp
@@ -33,7 +33,7 @@ protected:
 
     virtual void TearDown() //run after test
     {
-        if(m_serialport.isOpened())
+        if(m_serialport.isOpen())
         {
             m_serialport.close();
 
@@ -41,7 +41,7 @@ protected:
             imsleep(100);
         }
 
-        if(m_serialport2.isOpened())
+        if(m_serialport2.isOpen())
         {
             m_serialport2.close();
 
@@ -96,23 +96,23 @@ TEST_F(CSerialPortTests, open_1_3)
     EXPECT_TRUE(m_serialport2.open()) << m_availablePortsInfoVector[1].portName << " open error, error code: " << m_serialport.getLastError();
 }
 
-// #2_1 未打开的串口,isOpened()是否与open()返回值一致
-TEST_F(CSerialPortTests, isOpened_2_1)
+// #2_1 未打开的串口,isOpen()是否与open()返回值一致
+TEST_F(CSerialPortTests, isOpen_2_1)
 {
     m_serialport.init(m_availablePortsInfoVector[0].portName);
     bool isOpen = m_serialport.open();
-    EXPECT_TRUE(isOpen == m_serialport.isOpened()) << m_availablePortsInfoVector[0].portName << " function isOpened() isOpened status not equal function open() ";
+    EXPECT_TRUE(isOpen == m_serialport.isOpen()) << m_availablePortsInfoVector[0].portName << " function isOpen() isOpen status not equal function open() ";
 }
 
-// #2_2 被占用的串口,isOpened()是否与open()返回值一致
-TEST_F(CSerialPortTests, isOpened_2_2)
+// #2_2 被占用的串口,isOpen()是否与open()返回值一致
+TEST_F(CSerialPortTests, isOpen_2_2)
 {
     m_serialport2.init(m_availablePortsInfoVector[0].portName);
     ASSERT_TRUE(m_serialport2.open()) << m_availablePortsInfoVector[0].portName << " Used error";
 
     m_serialport.init(m_availablePortsInfoVector[0].portName);
     bool isOpen = m_serialport.open();
-    EXPECT_TRUE(isOpen == m_serialport.isOpened()) << m_availablePortsInfoVector[0].portName << " function isOpened() isOpened status not equal function open() ";
+    EXPECT_TRUE(isOpen == m_serialport.isOpen()) << m_availablePortsInfoVector[0].portName << " function isOpen() isOpen status not equal function open() ";
 }
 
 // #3_1 open port and close 打开并关闭串口
@@ -122,7 +122,7 @@ TEST_F(CSerialPortTests, open_close_3_1)
     EXPECT_TRUE(m_serialport.open()) << m_availablePortsInfoVector[0].portName << " open error, error code: " << m_serialport.getLastError();
 
     m_serialport.close();
-    EXPECT_FALSE(m_serialport.isOpened()) << m_availablePortsInfoVector[0].portName << " close error, error code: " << m_serialport.getLastError();
+    EXPECT_FALSE(m_serialport.isOpen()) << m_availablePortsInfoVector[0].portName << " close error, error code: " << m_serialport.getLastError();
 }
 
 // #3_2 open close and then open close 打开并关闭串口，再次打开并关闭串口
@@ -130,14 +130,14 @@ TEST_F(CSerialPortTests, open_close_3_2)
 {
     m_serialport.init(m_availablePortsInfoVector[0].portName);
     m_serialport.open();
-    EXPECT_TRUE(m_serialport.isOpened()) << m_availablePortsInfoVector[0].portName << " open error, error code: " << m_serialport.getLastError();
+    EXPECT_TRUE(m_serialport.isOpen()) << m_availablePortsInfoVector[0].portName << " open error, error code: " << m_serialport.getLastError();
     m_serialport.close();
-    EXPECT_FALSE(m_serialport.isOpened()) << m_availablePortsInfoVector[0].portName << " close error, error code: " << m_serialport.getLastError();
+    EXPECT_FALSE(m_serialport.isOpen()) << m_availablePortsInfoVector[0].portName << " close error, error code: " << m_serialport.getLastError();
     
     m_serialport.open();
-    EXPECT_TRUE(m_serialport.isOpened()) << m_availablePortsInfoVector[0].portName << " open again error, error code: " << m_serialport.getLastError();
+    EXPECT_TRUE(m_serialport.isOpen()) << m_availablePortsInfoVector[0].portName << " open again error, error code: " << m_serialport.getLastError();
     m_serialport.close();
-    EXPECT_FALSE(m_serialport.isOpened()) << m_availablePortsInfoVector[0].portName << " close again error, error code: " << m_serialport.getLastError();
+    EXPECT_FALSE(m_serialport.isOpen()) << m_availablePortsInfoVector[0].portName << " close again error, error code: " << m_serialport.getLastError();
 }
 
 // #3_3 打开并关闭串口(9600,N,1)，切换串口，再次打开串口(9600,N,1)	
@@ -147,15 +147,15 @@ TEST_F(CSerialPortTests, open_close_3_3)
 
     m_serialport.init(m_availablePortsInfoVector[0].portName);
     m_serialport.open();
-    EXPECT_TRUE(m_serialport.isOpened()) << m_availablePortsInfoVector[0].portName << " open error, error code: " << m_serialport.getLastError();
+    EXPECT_TRUE(m_serialport.isOpen()) << m_availablePortsInfoVector[0].portName << " open error, error code: " << m_serialport.getLastError();
     m_serialport.close();
-    EXPECT_FALSE(m_serialport.isOpened()) << m_availablePortsInfoVector[0].portName << " close error, error code: " << m_serialport.getLastError();
+    EXPECT_FALSE(m_serialport.isOpen()) << m_availablePortsInfoVector[0].portName << " close error, error code: " << m_serialport.getLastError();
     
     m_serialport.init(m_availablePortsInfoVector[1].portName);
     m_serialport.open();
-    EXPECT_TRUE(m_serialport.isOpened()) << m_availablePortsInfoVector[1].portName << " open again error, error code: " << m_serialport.getLastError();
+    EXPECT_TRUE(m_serialport.isOpen()) << m_availablePortsInfoVector[1].portName << " open again error, error code: " << m_serialport.getLastError();
     m_serialport.close();
-    EXPECT_FALSE(m_serialport.isOpened()) << m_availablePortsInfoVector[1].portName << " close again error, error code: " << m_serialport.getLastError();
+    EXPECT_FALSE(m_serialport.isOpen()) << m_availablePortsInfoVector[1].portName << " close again error, error code: " << m_serialport.getLastError();
 }
 
 // #3_4 打开并关闭串口(9600,N,1)，切换波特率，再次打开串口(115200,N,1)		
@@ -163,15 +163,15 @@ TEST_F(CSerialPortTests, open_close_3_4)
 {
     m_serialport.init(m_availablePortsInfoVector[0].portName,9600);
     m_serialport.open();
-    EXPECT_TRUE(m_serialport.isOpened()) << m_availablePortsInfoVector[0].portName << " open error, error code: " << m_serialport.getLastError();
+    EXPECT_TRUE(m_serialport.isOpen()) << m_availablePortsInfoVector[0].portName << " open error, error code: " << m_serialport.getLastError();
     m_serialport.close();
-    EXPECT_FALSE(m_serialport.isOpened()) << m_availablePortsInfoVector[0].portName << " close error, error code: " << m_serialport.getLastError();
+    EXPECT_FALSE(m_serialport.isOpen()) << m_availablePortsInfoVector[0].portName << " close error, error code: " << m_serialport.getLastError();
     
     m_serialport.init(m_availablePortsInfoVector[0].portName,115200);
     m_serialport.open();
-    EXPECT_TRUE(m_serialport.isOpened()) << m_availablePortsInfoVector[0].portName << " open again error, error code: " << m_serialport.getLastError();
+    EXPECT_TRUE(m_serialport.isOpen()) << m_availablePortsInfoVector[0].portName << " open again error, error code: " << m_serialport.getLastError();
     m_serialport.close();
-    EXPECT_FALSE(m_serialport.isOpened()) << m_availablePortsInfoVector[0].portName << " close again error, error code: " << m_serialport.getLastError();
+    EXPECT_FALSE(m_serialport.isOpen()) << m_availablePortsInfoVector[0].portName << " close again error, error code: " << m_serialport.getLastError();
 }
 
 } // namespace
