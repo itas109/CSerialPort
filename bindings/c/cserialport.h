d4e57c626bbfd8fe0c814834188e6be333d17598
itas109 <itas109@qq.com>
2025-04-01 23:26:56 +0800

    feat(bindings/c): fixed #97 mingw compile error

--- bindings/c/cserialport.h
+++ bindings/c/cserialport.h
@@ -7,7 +7,11 @@ typedef unsigned __int64 i_handle_t; // windows 64bit
 #else
 typedef unsigned int i_handle_t; // windows 32bit
 #endif
-#elif defined(__LP64__) || defined(_LP64)
+#elif defined(__MINGW64__) // MinGW-w64 (64-bit)
+typedef unsigned long long i_handle_t; // windows 64bit (MinGW-w64)
+#elif defined(__MINGW32__) // MinGW (32-bit)
+typedef unsigned int i_handle_t; // windows 32bit (MinGW)
+#elif defined(__LP64__) || defined(_LP64) // gcc/clang
 typedef unsigned long i_handle_t; // LP64(long pointer 64) // 64bit OS(unix/linux/macos)
 #else
 typedef unsigned int i_handle_t; // ILP32(int long pointer 32) // 32bit OS(windows/unix/linux/macos)
